4bd5ee4c6be636213ad35d4b06b9b108b2f0fe6a
kimth007kim <kimth007kim@naver.com>
2021-06-08 19:26:14 +0900

    스프링 웹 개발 기초-API 완료

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,4 +19,34 @@ public class HelloController {
         model.addAttribute("name",name);        // 파라미터로 넘어온 name을 넘겨보자 "name"이 key이고 name이 value이다.
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody                                                            //http에서 body부에서 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " +name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+
+    static class Hello{     //자바빈 규약 private이어서 바로 못꺼내는데 method로 꺼낸다. java Bean 표준 방식
+        private String name;
+
+        public String getName() {       //Property 접근방식
+            return name;
+        }
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 }
