fd429b5a4e827907940517a272a989cc25d6f8c5
kimth007kim <kimth007kim@naver.com>
2021-06-28 04:05:08 +0900

    스프링 DB 접근 기술- JPA

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.springframework.test.annotation.Commit;
 
 import java.util.Optional;
 
