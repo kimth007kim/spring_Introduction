a25bc70fa2398e0c546f1fbf8e6a06320764e82a
kimth007kim <kimth007kim@naver.com>
2021-06-29 00:16:13 +0900

    AOP-AOP 적용

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,8 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
+
+
 
 //    @Bean
 //    public MemberRepository memberRepository(){
