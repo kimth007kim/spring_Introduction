d399dd61354f7fdb4eb69ddf69d0c0f78e12faca
kimth007kim <kimth007kim@naver.com>
2021-07-09 15:56:36 +0900

    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 --프로젝트 환경설정--  엔티티 설계시 주의점 완료

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -7,6 +7,8 @@ import org.apache.tomcat.jni.Address;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class Delivery {
@@ -15,7 +17,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery",fetch = LAZY)
     private Order order;
 
     @Embedded
