293cfa3a4a1149648207f88a74b7b75fd83cf075
kimth007kim <kimth007kim@naver.com>
2021-07-26 02:01:14 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/ API 개발 기본 / 회원 등록 API 완료

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController //@ResponseBody+ @Controller
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id =memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+//    요청이온것을 request body로 creatememberRequest에 바인딩이되고 엔티티와 프레젠테이션과 분리할수있다.
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+
+    }
+    @Data
+    static class CreateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
