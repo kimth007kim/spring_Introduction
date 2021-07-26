ca5b2c2a0e496dc7c40b758f90323dbe6bb8de03
kimth007kim <kimth007kim@naver.com>
2021-07-26 18:52:49 +0900

    실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/ API 개발 기본 / 회원 조회 API 완료

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController //@ResponseBody+ @Controller
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    //조회 v1: 응답값으로 엔티티를 전부 외부에 노출
+    @GetMapping("/api/v1/members")  //제일 단순한 방법으로 조회하기
+    public List<Member> membersV1(){
+            return memberService.findMembers();  //가져와서 리스트로 엔티티를 반환하면 끝난다.
+    }                                           //엔티티의 정보를 노출하면 엔티티의 모든 정보가 외부에 노출된다!!!!!
+                                                //모든케이스에 대응할수 없다.
+
+    //조회 v2: 응답 값으로 엔티티가 아닌 별도의 DTO 사용
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){           //엔티티를 새로운 배열로 만들어서 리턴하는 방법
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,12 +17,12 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
-    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore         //orders 정보를 노출하지 않기 위해서 추가한 어노테이션이다. 엔티티에 프레젠티에션(화면에 출력)되는 로직이 추가된것이다. 이렇게되면 엔티티가 의존관계가 나가버린것이다.
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
