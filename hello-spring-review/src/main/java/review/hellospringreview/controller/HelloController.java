253eaefcbef19117f776dbece34ef8f9b302f666
kimth007kim <kimth007kim@naver.com>
2021-12-13 18:24:10 +0900

    스프링부트 /복습 /  도메인과 리포지토리 만들기 완료

--- hello-spring-review/src/main/java/review/hellospringreview/controller/HelloController.java
+++ hello-spring-review/src/main/java/review/hellospringreview/controller/HelloController.java
@@ -4,13 +4,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+
+    //RequestBody 객체 반환하는 법법
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
     @GetMapping("hello-mvc")
     public String hello(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
 }
---
+++ hello-spring-review/src/main/java/review/hellospringreview/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package review.hellospringreview.repository;
+
+import review.hellospringreview.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
